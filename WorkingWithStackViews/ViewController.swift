2b475c86aff301f9cc3e5ff5bd419fa8734fe154
sinahk <sinahk1992@yahoo.com>
2019-02-13 20:29:41 +0330

    Initial Commit

---
+++ WorkingWithStackViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithStackViews
+//
+//  Created by sinahk on 2/13/19.
+//  Copyright © 2019 ir.sinakhodadad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
